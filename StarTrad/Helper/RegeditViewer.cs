7e7c1784ec44a928058f6699ca2a4f51abfc4c7c
Dercraker <antoine.capitain@gmail.com>
2023-12-30 00:51:41 +0100

    Add Regedit Viewer

---
+++ StarTrad/Helper/RegeditViewer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32;
+
+namespace StarTrad.Helper;
+
+public static class RegeditViewer
+{
+    public static object? GetRegeditValue(string registryKeyPath, string registryKeyName)
+    {
+        object registryValue = Registry.GetValue(registryKeyPath, registryKeyName, null);
+        if (registryValue is not null)
+            return registryValue;
+        else
+            return null;
+    }
+}
--- StarTrad/Tool/TranslationVersion.cs
+++ StarTrad/Tool/TranslationVersion.cs
@@ -1,82 +1,84 @@
 ﻿namespace StarTrad.Tool
 {
-	/// <summary>
-	/// Parses the version number of a translation, such as "3.22.0-1.0-20231226".
-	/// </summary>
-	internal class TranslationVersion
-	{
-		public TranslationVersion(string part0, string part1, string part2)
-		{
-			this.TargetedGameVersion = part0;
-			this.VersionNumber = part1;
-			this.BuildNumber = part2;
-		}
+    /// <summary>
+    /// Parses the version number of a translation, such as "3.22.0-1.0-20231226".
+    /// </summary>
+    internal class TranslationVersion
+    {
+        public TranslationVersion(string part0, string part1, string part2)
+        {
+            this.TargetedGameVersion = part0;
+            this.VersionNumber = part1;
+            this.BuildNumber = part2;
+        }
 
-		/*
-		Static
-		*/
+        #region Static
 
-		public static TranslationVersion Make(string version)
-		{
-			string[] parts = version.Trim().Split('-');
+        public static TranslationVersion Make(string version)
+        {
+            string[] parts = version.Trim().Split('-');
 
-			if (parts.Length != 3) {
-				return null;
-			}
+            if (parts.Length != 3)
+            {
+                return null;
+            }
 
-			return new TranslationVersion(parts[0], parts[1], parts[2]);
-		}
+            return new TranslationVersion(parts[0], parts[1], parts[2]);
+        }
 
-		/*
-		Public
-		*/
+        #endregion
 
-		public bool Equals(TranslationVersion other)
-		{
-			return this.TargetedGameVersion == other.TargetedGameVersion
-				&& this.VersionNumber == other.VersionNumber
-				&& this.BuildNumber == other.BuildNumber;
-		}
+        #region Public
 
-		public bool IsNewerThan(TranslationVersion other)
-		{
-			int thisBuildNumber;
-			int otherBuildNumber;
+        public bool Equals(TranslationVersion other)
+        {
+            return this.TargetedGameVersion == other.TargetedGameVersion
+                && this.VersionNumber == other.VersionNumber
+                && this.BuildNumber == other.BuildNumber;
+        }
 
-			if (!int.TryParse(this.BuildNumber, out thisBuildNumber)
-			||  !int.TryParse(other.BuildNumber, out otherBuildNumber)) {
-				return false;
-			}
+        public bool IsNewerThan(TranslationVersion other)
+        {
+            int thisBuildNumber;
+            int otherBuildNumber;
 
-			return thisBuildNumber > otherBuildNumber;
-		}
+            if (!int.TryParse(this.BuildNumber, out thisBuildNumber)
+            || !int.TryParse(other.BuildNumber, out otherBuildNumber))
+            {
+                return false;
+            }
 
-		/*
-		Accessor
-		*/
+            return thisBuildNumber > otherBuildNumber;
+        }
 
-		/// <summary>
-		/// Returns the version of the game the translations is made for, like "3.22.0".
-		/// </summary>
-		public string TargetedGameVersion
-		{
-			get; internal set;
-		}
+        #endregion
 
-		/// <summary>
-		/// Returns the translation's version number, such as "1.0".
-		/// </summary>
-		public string VersionNumber
-		{
-			get; internal set;
-		}
+        #region Accessor
 
-		/// <summary>
-		/// Returns the translation's build number, such as "20231226".
-		/// </summary>
-		public string BuildNumber
-		{
-			get; internal set;
-		}
-	}
+        /// <summary>
+        /// Returns the version of the game the translations is made for, like "3.22.0".
+        /// </summary>
+        public string TargetedGameVersion
+        {
+            get; internal set;
+        }
+
+        /// <summary>
+        /// Returns the translation's version number, such as "1.0".
+        /// </summary>
+        public string VersionNumber
+        {
+            get; internal set;
+        }
+
+        /// <summary>
+        /// Returns the translation's build number, such as "20231226".
+        /// </summary>
+        public string BuildNumber
+        {
+            get; internal set;
+        }
+
+        #endregion
+    }
 }
