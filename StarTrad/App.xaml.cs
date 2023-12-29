7d7202acabb5dde61f7b79fcaea08c4938d249b4
nostrenz <nostrenz@gmail.com>
2023-12-29 18:37:28 +0100

    Update project to use .NET 8.0

--- StarTrad/App.xaml.cs
+++ StarTrad/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using StarTrad.Helper;
+﻿using System.Windows;
+using StarTrad.Helper;
 using StarTrad.Tool;
-using System;
-using System.Drawing;
-using System.Reflection;
-using System.Windows.Forms;
 
 namespace StarTrad
 {
@@ -13,11 +10,20 @@ namespace StarTrad
 	public partial class App : System.Windows.Application
 	{
 		// Full path to the location where this program is running
-		public static string workingDirectoryPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)) + @"\";
+		public static string workingDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
+
+		private static ApplicationContext applicationContext = new ApplicationContext();
+		private static NotifyIcon notifyIcon = new NotifyIcon();
 
 		public App() : base()
 		{
+			ApplicationConfiguration.Initialize();
+
+			this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
 			this.CreateNotifyIcon();
+
+			System.Windows.Forms.Application.Run(applicationContext);
+
 			LoggerFactory.Setup();
 			LoggerFactory.LogInformation("Démarrage de StarTrad");
 		}
@@ -31,57 +37,28 @@ namespace StarTrad
 		/// </summary>
 		private void CreateNotifyIcon()
 		{
-			ContextMenu contextMenu = new ContextMenu();
+			ContextMenuStrip cms = new ContextMenuStrip();
+			cms.Items.Add(new ToolStripMenuItem("Vérifier et installer traduction", null, new EventHandler(this.UpdateMenuItem_Click)));
+			cms.Items.Add(new ToolStripMenuItem("Vérifier, installer traduction et lancer", null, new EventHandler(this.UpdateAndLaunchMenuItem_Click)));
+			cms.Items.Add(new ToolStripSeparator());
+			cms.Items.Add(new ToolStripMenuItem("Options avancées", null, new EventHandler(this.SettingsMenuItem_Click)));
+			cms.Items.Add(new ToolStripMenuItem("Quitter", null, new EventHandler(this.ExitMenuItem_Click)));
 
-			MenuItem updateMenuItem = new MenuItem();
-			updateMenuItem.Text = "Vérifier et installer traduction";
-			updateMenuItem.Click += new EventHandler(this.UpdateMenuItem_Click);
-
-			MenuItem updateAndLaunchMenuItem = new MenuItem();
-			updateAndLaunchMenuItem.Text = "Vérifier, installer traduction et lancer";
-			updateAndLaunchMenuItem.Click += new EventHandler(this.UpdateAndLaunchMenuItem_Click);
-
-			MenuItem settingsMenuItem = new MenuItem();
-			settingsMenuItem.Text = "Options avancées";
-			settingsMenuItem.Click += new EventHandler(this.SettingsMenuItem_Click);
-
-			MenuItem exitMenuItem = new MenuItem();
-			exitMenuItem.Text = "Quitter";
-			exitMenuItem.Click += new EventHandler(this.ExitMenuItem_Click);
-
-			contextMenu.MenuItems.AddRange(new MenuItem[] {
-				updateMenuItem,
-				updateAndLaunchMenuItem,
-				settingsMenuItem,
-				exitMenuItem
-			});
-
-			NotifyIcon notifyIcon = new NotifyIcon(new System.ComponentModel.Container());
-			notifyIcon.Icon = new Icon("StarTrad.ico");
+			notifyIcon.ContextMenuStrip = cms;
+			notifyIcon.Icon = new Icon(workingDirectoryPath + @"\StarTrad.ico");
 			notifyIcon.Visible = true;
-			notifyIcon.ContextMenu = contextMenu;
-		}
-
-		/// <summary>
-		/// Attemps to find the path to the RSI Launcher's Library Folder then displays the result.
-		/// </summary>
-		private void FindAndShowLibraryFolderPath()
-		{
-			string folderPath = LibraryFolderFinder.GetRsiLauncherLibraryFolderPath();
-
-			MessageBox.Show(folderPath != null ? folderPath : "Chemin du Library Folder non trouvé.");
-			LoggerFactory.LogWarning("Chemin du Library Folder non trouvé");
 		}
 
 		/*
 		Event
 		*/
+
 		/// <summary>
 		/// Called when clicking on the "Update" tray menu item.
 		/// </summary>
-		/// <param name="Sender"></param>
+		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		private void UpdateMenuItem_Click(object Sender, EventArgs e)
+		private void UpdateMenuItem_Click(object? sender, EventArgs e)
 		{
 			TranslationInstaller installer = new TranslationInstaller();
 			installer.InstallLatestTranslation();
@@ -90,9 +67,9 @@ namespace StarTrad
 		/// <summary>
 		/// Called when clicking on the "Update & Launch" tray menu item.
 		/// </summary>
-		/// <param name="Sender"></param>
+		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		private void UpdateAndLaunchMenuItem_Click(object Sender, EventArgs e)
+		private void UpdateAndLaunchMenuItem_Click(object? sender, EventArgs e)
 		{
 			TranslationInstaller installer = new TranslationInstaller();
 			installer.InstallLatestTranslation();
@@ -101,9 +78,9 @@ namespace StarTrad
 		/// <summary>
 		/// Called when clicking on the "Settings" tray menu item.
 		/// </summary>
-		/// <param name="Sender"></param>
+		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		private void SettingsMenuItem_Click(object Sender, EventArgs e)
+		private void SettingsMenuItem_Click(object? sender, EventArgs e)
 		{
 			LoggerFactory.LogInformation("Ouverture des paramètres");
 			View.Window.Settings settingsWindow = new View.Window.Settings();
@@ -113,12 +90,16 @@ namespace StarTrad
 		/// <summary>
 		/// Called when clicking on the "Exit" tray menu item.
 		/// </summary>
-		/// <param name="Sender"></param>
+		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		private void ExitMenuItem_Click(object Sender, EventArgs e)
+		private void ExitMenuItem_Click(object? sender, EventArgs e)
 		{
 			LoggerFactory.LogInformation("Fermeture de StarTrad");
-			this.Shutdown();
+
+			applicationContext.ExitThread();
+			notifyIcon.Visible = false;
+
+			System.Windows.Forms.Application.Exit();
 		}
 	}
 }
--- StarTrad/Helper/LoggerFactory.cs
+++ StarTrad/Helper/LoggerFactory.cs
@@ -3,31 +3,31 @@ using System.Diagnostics;
 
 namespace StarTrad.Helper
 {
-    public static class LoggerFactory
-    {
-        private static TextWriterTraceListener fileListener = new TextWriterTraceListener(App.workingDirectoryPath + @"logfile.txt");
-        public static void Setup()
-        {
-            Trace.Listeners.Add(fileListener);
-            Trace.WriteLine("\n");
-        }
+	public static class LoggerFactory
+	{
+		private static TextWriterTraceListener fileListener = new TextWriterTraceListener(App.workingDirectoryPath + @"logfile.txt");
+		public static void Setup()
+		{
+			Trace.Listeners.Add(fileListener);
+			Trace.WriteLine("\n");
+		}
 
-        public static void LogInformation(string message)
-        {
-            Trace.TraceInformation($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [INFO] {message}");
-            Trace.Flush();
-        }
+		public static void LogInformation(string message)
+		{
+			Trace.TraceInformation($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [INFO] {message}");
+			Trace.Flush();
+		}
 
-        public static void LogWarning(string message)
-        {
-            Trace.TraceWarning($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [WARN] {message}");
-            Trace.Flush();
-        }
+		public static void LogWarning(string message)
+		{
+			Trace.TraceWarning($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [WARN] {message}");
+			Trace.Flush();
+		}
 
-        public static void LogError(Exception exception)
-        {
-            Trace.TraceError($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [ERROR] {exception.Message}\n{exception}");
-            Trace.Flush();
-        }
-    }
+		public static void LogError(Exception exception)
+		{
+			Trace.TraceError($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [ERROR] {exception.Message}\n{exception}");
+			Trace.Flush();
+		}
+	}
 }
